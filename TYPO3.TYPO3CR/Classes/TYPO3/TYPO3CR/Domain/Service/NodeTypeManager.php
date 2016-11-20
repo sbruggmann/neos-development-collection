8a29f91579c4ef72e6893d988381a03be75b478d
Stefan Bruggmann <stefan.bruggmann@dotpulse.ch>
2016-11-20 17:53:09 +0100

    Enable inheritance for nodetype properties

--- TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Domain/Service/NodeTypeManager.php
+++ TYPO3.TYPO3CR/Classes/TYPO3/TYPO3CR/Domain/Service/NodeTypeManager.php
@@ -188,6 +188,38 @@ class NodeTypeManager
 
         $completeNodeTypeConfiguration = $this->configurationManager->getConfiguration('NodeTypes');
 
+        $completeNodeTypeConfigurationOverwrite = array();
+        foreach ($completeNodeTypeConfiguration as $nodeTypeName => $nodeTypeConfiguration) {
+            $superTypesProperties = array();
+            if (isset($nodeTypeConfiguration['superTypes'])) {
+                foreach ($nodeTypeConfiguration['superTypes'] as $superType => $enabled) {
+                    if ($enabled && isset($completeNodeTypeConfiguration[$superType]['properties'])) {
+                        foreach ($completeNodeTypeConfiguration[$superType]['properties'] as $superTypePropertyName => $superTypePropertyConfiguration) {
+                            $superTypesProperties[$superTypePropertyName] = $superTypePropertyConfiguration;
+                        }
+                    }
+                }
+            }
+            if (array_key_exists('properties', $nodeTypeConfiguration)) {
+                foreach ($nodeTypeConfiguration['properties'] as $propertyName => $propertyConfiguration) {
+                    if (strpos($propertyName, ' < ')!==false) {
+                        $explodedPropertyName = explode(' < ', $propertyName);
+                        if (count($explodedPropertyName) > 0) {
+                            $completeNodeTypeConfigurationOverwrite[$nodeTypeName]['properties'][$explodedPropertyName[0]] = Arrays::arrayMergeRecursiveOverrule(
+                                Arrays::arrayMergeRecursiveOverrule(
+                                    array_key_exists($explodedPropertyName[1], $superTypesProperties) ? $superTypesProperties[$explodedPropertyName[1]] : array(),
+                                    $propertyConfiguration
+                                ),
+                                isset($completeNodeTypeConfiguration[$nodeTypeName]['properties'][$explodedPropertyName[0]]) ? $completeNodeTypeConfiguration[$nodeTypeName]['properties'][$explodedPropertyName[0]] : array()
+                            );
+                            unset($completeNodeTypeConfiguration[$nodeTypeName]['properties'][$propertyName]);
+                        }
+                    }
+                }
+            }
+        }
+        $completeNodeTypeConfiguration = Arrays::arrayMergeRecursiveOverrule($completeNodeTypeConfiguration, $completeNodeTypeConfigurationOverwrite);
+
         foreach (array_keys($completeNodeTypeConfiguration) as $nodeTypeName) {
             if (!is_array($completeNodeTypeConfiguration[$nodeTypeName])) {
                 continue;
